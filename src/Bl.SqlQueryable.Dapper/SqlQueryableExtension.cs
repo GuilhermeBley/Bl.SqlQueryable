1b0c5da795f83cedb55de2035a5c28f9797d37fb
GuilhermeBley <guilhermecbley@gmail.com>
2023-06-10 12:41:38 -0300

    async query

--- src/Bl.SqlQueryable.Dapper/SqlQueryableExtension.cs
+++ src/Bl.SqlQueryable.Dapper/SqlQueryableExtension.cs
@@ -5,7 +5,7 @@ namespace Bl.SqlQueryable.Dapper;
 
 public static class SqlQueryableExtension
 {
-    public static IEnumerable<T> Query<T>(this IDbConnection cnn, string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+    public static IQueryable<T> Queryable<T>(this IDbConnection cnn, string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null)
     {
         var dynamicParameters = new DynamicParameters(param);
 
@@ -18,22 +18,9 @@ public static class SqlQueryableExtension
             commandTimeout: commandTimeout,
             commandType: commandType);
 
-        return queryable.Provider.Execute<IEnumerable<T>>(queryable.Expression);
+        return queryable;
     }
 
-    public static async Task<IEnumerable<T>> QueryAsync<T>(this IDbConnection cnn, string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null)
-    {
-        var dynamicParameters = new DynamicParameters(param);
-
-        var queryable = new BuildersQueryable<T>(
-            query: sql,
-            connection: cnn,
-            dynamicParameters: dynamicParameters,
-            transaction: transaction,
-            buffered: true,
-            commandTimeout: commandTimeout,
-            commandType: commandType);
-
-        return await ((IAsyncQueryProvider)queryable.Provider).ExecuteAsync<IEnumerable<T>>(queryable.Expression);
-    }
+    public static async Task<IEnumerable<T>> QueryAsync<T>(BuildersQueryable<T> queryable, CancellationToken cancellationToken = default)
+        => await ((IAsyncQueryProvider)queryable.Provider).ExecuteAsync<IEnumerable<T>>(queryable.Expression, cancellationToken: cancellationToken);
 }
