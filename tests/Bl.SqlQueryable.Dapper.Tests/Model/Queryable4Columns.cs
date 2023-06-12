ef4897f0391a02e5d3c67317652ae9681431590c
GuilhermeBley <guilhermecbley@gmail.com>
2023-06-11 21:16:17 -0300

    Tests connect to mysql

---
+++ tests/Bl.SqlQueryable.Dapper.Tests/Model/Queryable4Columns.cs
@@ -0,0 +1,9 @@
+namespace Bl.SqlQueryable.Dapper.Tests.Model;
+
+public class Queryable4Columns
+{
+    public int Id { get; set; }
+    public string Col2 { get; set; } = string.Empty;
+    public DateTime Col3 { get; set; }
+    public double Col4 { get; set; }
+}
--- tests/Bl.SqlQueryable.Dapper.Tests/Tests/QueryableTest.cs
+++ tests/Bl.SqlQueryable.Dapper.Tests/Tests/QueryableTest.cs
@@ -1,14 +1,16 @@
 using System.Data;
 using Bl.SqlQueryable.Dapper;
+using Bl.SqlQueryable.Dapper.Tests.Model;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Bl.SqlQueryable.Dapper.Tests.Tests;
 
 public class QueryableTest : DefaultHost
 {
     [Fact]
-    public async Task Test1()
+    public async Task Queryable_GetList_Success()
     {
-        IDbConnection c = null!;
-        
+        IDbConnection c = CreateScope().GetRequiredService<IDbConnection>();
+        var result = await c.Queryable<Queryable4Columns>("SELECT * FROM queryable.queryable4columns;").ToListAsync();
     }
 }
